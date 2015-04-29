5e65820358b8b69321f0ccd4c829c33c50fae243
David Moreno García <david.mogar@gmail.com>
2015-04-29 20:29:55 +0200

    Add code to classify tokens and guess the gender
    Some functions have been added to tokenize input text and classify each item as name or surname. After this, the gender is guessed using names file and a JSON string is generated with all computed information.

--- genderator/genderator.py
+++ genderator/genderator.py
@@ -1,73 +1,154 @@
+import codecs
+import json
 import os
+
+from collections import OrderedDict
+from .utils import Normalizer
 
 path = os.path.dirname(__file__)
 
 
 class Genderator:
-
     __names, __ratios = {}, {}
     __surnames = []
 
     def __init__(self):
-        self.__load_data()
+        self._load_data()
 
-    def __load_data(self):
+    def _load_data(self):
         """
         Load all data files into memory.
         """
-        self.__load_names()
-        self.__load_name_surname_ratios()
-        self.__load_surnames()
+        self._load_names()
+        self._load_name_surname_ratios()
+        self._load_surnames()
 
-    def __load_names(self):
+    def _load_names(self):
         """
         Load names data file.
 
         This file contains a list of spanish given names with the probability for
         each one to be a male or female name.
         """
-        with open(os.path.join(path, 'data/names_ine')) as file:
-            for line in file:
-                if not line.startswith('#'):
-                    (name, prob_female, prob_male) = line.rstrip().split('\t')
-                    self.__names[name] = float(prob_male)
+        for line in self.remove_file_comments('data/names_ine'):
+                (name, prob_female, prob_male) = line.rstrip().split('\t')
+                self.__names[name] = float(prob_male)
 
-    def __load_name_surname_ratios(self):
+    def _load_name_surname_ratios(self):
         """
         Load name/surnames ratios data file.
 
         The file contains a list of names and surnames with the probability for each
         one to be a name (lower values) or a surname (higher values).
         """
-        with open(os.path.join(path, 'data/name_surname_ratio')) as file:
-            for line in file:
-                if not line.startswith('#'):
-                    (key, val) = line.rstrip().split('\t')
-                    self.__ratios[key] = float(val)
+        for line in self.remove_file_comments('data/name_surname_ratio'):
+            (key, val) = line.rstrip().split('\t')
+            self.__ratios[key] = float(val)
 
-    def __load_surnames(self):
+    def _load_surnames(self):
         """
         Load names data file.
 
         This file contains a list of spanish surnames.
         """
-        with open(os.path.join(path, 'data/surnames_ine')) as file:
-            for line in file:
-                if not line.startswith('#'):
-                    self.__surnames.append(line.rstrip())
+        for line in self.remove_file_comments('data/surnames_ine'):
+            self.__surnames.append(line.rstrip())
 
-    def parse(self, fullname):
-        return self.__normalize(fullname)
-
-    def __normalize(self, text):
+    def remove_file_comments(self, relative_path):
         """
-        Normalize input text transforming it to lowercase, removing hyphens,
-        whitespaces from both sides and extra whitespaces between words.
+        Generator to remove comments from a file.
 
         Params:
-            text: String to be normalized.
+            file: File to be processed.
+        """
+        with codecs.open(os.path.join(path, relative_path), 'r', 'UTF-8') as file:
+            for line in file:
+                line = line.strip()
+                if not line.startswith('#'):
+                    yield line
+
+    def guess_gender(self, fullname):
+        if isinstance(fullname, str):
+            fullname = Normalizer.normalize(fullname)
+            names, surnames = self._classify(fullname)
+
+            if names and surnames:
+                real_name, ratio = self._get_gender_ratio(list(names.keys()))
+                return self._create_answer(real_name, ratio, names, surnames)
+            pass
+
+    def _classify(self, fullname):
+        """
+        Classify fullname tokens into names and surnames based on datasets.
+
+        Params:
+            fullname: Full name to be classified.
 
         Returns:
-            Normalized text.
+            Two OrderedDicts of names and surnames with its confidence ratio.
         """
-        return ' '.join(text.lower().replace('-', ' ').strip().split())
+        name_complete = False
+        names, surnames = OrderedDict(), OrderedDict()
+
+        for word in fullname.split():
+            if word in self.__ratios:
+                # If word could be a name or surname
+                ratio = self.__ratios[word]
+                if ratio < 0.5 and not name_complete:
+                    names[word] = 1 - ratio
+                else:
+                    surnames[word] = ratio
+                    if ratio == 1:
+                        name_complete = True
+            else:
+                if word in self.__names:
+                    if not name_complete:
+                        names[word] = 1
+                elif word in self.__surnames:
+                    surnames[word] = 1
+                    name_complete = True
+
+        return names, surnames
+
+    def _get_gender_ratio(self, names):
+        """
+        Returns the male/female ratio for the given names.
+
+        To do this, the function compute possible names combining items on the list
+        and try to form the longest name possible.
+
+        The value returned go from 0 to 1. Values near to 1 represent a higher possibility
+        of the evaluated name to be a male name.
+
+        Params:
+            names: List of names.
+
+        Returns:
+            The longest name computed by combining items in the list,
+            and the male/female ratio.
+        """
+        for i in range(len(names), 0, -1):
+            real_name = ' '.join(names[:i])
+            if real_name in self.__names:
+                return real_name, self.__names[real_name]
+
+    def _create_answer(self, real_name, ratio, names, surnames):
+        """
+        Process computed data and generated a JSON answer.
+
+        Params:
+            real_name: Real name (computed name) extracted from the original text.
+            ratio: Male/female ratio.
+            names: Names identified on the original text.
+            surnames: Surnames identified on the original text.
+
+        Returns:
+            A JSON string with all the computed information.
+        """
+        answer = OrderedDict()
+        answer['names'] = names
+        answer['surnames'] = surnames
+        answer['real_name'] = real_name
+        answer['gender'] = 'Male' if ratio > 0.5 else 'Female'
+        answer['confidence'] = ratio
+        return json.dumps(answer)
