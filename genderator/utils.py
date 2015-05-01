4879a7c156a22ac7b354fe607e9fe4a91c8dfa6c
David Moreno García <david.mogar@gmail.com>
2015-05-01 22:50:10 +0200

    Change good accents to unicode values
     Travis seems to have a problem reading previous syntax.

--- genderator/utils.py
+++ genderator/utils.py
@@ -60,8 +60,8 @@ class Normalizer:
             The text without accent marks.
         """
         good_accents = {
-            u'\N{COMBINING TILDE}',
-            u'\N{COMBINING CEDILLA}'
+            u'\u0303',  # Combining tilde
+            u'\u0327'   # Cedilla
         }
 
         return ''.join(c for c in unicodedata.normalize('NFKD', text)
