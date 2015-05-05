3e53702026c8c16dfd2f24cb402a52c4fb61434b
David Moreno García <david.mogar@gmail.com>
2015-05-05 16:06:34 +0200

    Add long description to setup file

--- setup.py
+++ setup.py
@@ -10,9 +10,13 @@ version = re.search(
     open('genderator/__init__.py').read(),
     re.M).group(1)
 
+with open("README.rst", "rb") as f:
+    long_description = f.read().decode("utf-8")
+
 setup(name='genderator',
       version=version,
       description='Python library to guess gender given a spanish full name',
+      long_description=long_description,
       author='David Moreno-Garcia',
       author_email='david.mogar@gmail.com',
       license='MIT',
