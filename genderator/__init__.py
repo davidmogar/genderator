bd970107b838efbd1844394729fad1c5b211ce55
David Moreno García <david.mogar@gmail.com>
2015-05-07 09:02:32 +0200

    Add a new step to normalization
    I was removing hyphens instead of replacing them.

--- genderator/__init__.py
+++ genderator/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.2.7.6'
+__version__ = '0.2.7.7'
 
 from genderator.parser import Parser
--- genderator/parser.py
+++ genderator/parser.py
@@ -100,6 +100,7 @@ class Parser:
         if isinstance(fullname, str):
             if self.__normalize:
                 fullname = self.__normalizr.remove_extra_whitespaces(fullname)
+                fullname = self.__normalizr.replace_hyphens(fullname)
                 fullname = self.__normalizr.remove_symbols(fullname, 'NFKC', self.__normalizr_exclusions)
                 fullname = self.__normalizr.remove_punctuation(fullname, excluded=set('\'')).lower()
 
